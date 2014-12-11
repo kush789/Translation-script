5e2b886a8d3de66a868d3fb7d2e8ad2f51f1598a
kush789 <singh.kushagra.1996@gmail.com>
2014-12-12 04:35:53 +0530

    Added full list of supported languages

--- validate.py
+++ validate.py
@@ -1,24 +1,55 @@
 def takelanguages():
 	chechklang1 = 1
 	chechklang2 = 1
-	langset = ["ru","en","pl","uk","de","fr","es","it","bg","cs","tr","ro","se"]
+	langset = ['ca', 'cs', 'pt', 'tr', 'lv', 'lt', 'th', 'pl', 'hy', 'hr', 'de', 'da', 'he', 'mk', 'mt', 'uk', 'el', 'en', 'zh', 'vi', 'is', 'it', 'ar', 'et', 'az', 'id', 'es', 'ru', 'nl', 'no', 'ro', 'be', 'fr', 'bg', 'ms', 'bs', 'fi', 'hu', 'ka', 'sr', 'sq', 'sv', 'sk', 'sl']
 	while (chechklang1):
 		print """
 Select language input text is in : (Enter the correct code please)
 
-	ru : Russian
-	en : English
-	pl : Polska
-	uk : Ukrainian
-	de : German
-	fr : French
-	es : spanish
-	it : Italian
-	bg : Bulgarian
-	cs : czech
-	tr : Turkish
-	ro : Romanian
-	sr : Serbian
+ -  ar Arabic
+ -  az Azerbaijani
+ -  be Belarusian
+ -  bg Bulgarian
+ -  bs Bosnian
+ -  ca Catalan
+ -  cs Czech
+ -  da Danish
+ -  de German
+ -  el Greek
+ -  en English
+ -  es Spanish
+ -  et Estonian
+ -  fi Finnish
+ -  fr French
+ -  he Hebrew
+ -  hr Croatian
+ -  hu Hungarian
+ -  hy Armenian
+ -  id Indonesian
+ -  is Icelandic
+ -  it Italian
+ -  ka Georgian
+ -  lt Lithuanian
+ -  lv Latvian
+ -  mk Macedonian
+ -  ms Malay
+ -  mt Maltese
+ -  nl Dutch
+ -  no Norwegian
+ -  pl Polish
+ -  pt Portuguese
+ -  ro Romanian
+ -  ru Russian
+ -  sk Slovak
+ -  sl Slovenian
+ -  sq Albanian
+ -  sr Serbian
+ -  sv Swedish
+ -  th Thai
+ -  tr Turkish
+ -  uk Ukrainian
+ -  vi Vietnamese
+ -  zh Chinese
 
 ==================================================	"""
 		fromlang = raw_input()
@@ -30,19 +61,50 @@ Select language input text is in : (Enter the correct code please)
 		print """
 Select language you want to translate to : (Enter the correct code please)
 
-	ru : Russian
-	en : English
-	pl : Polska
-	uk : Ukrainian
-	de : German
-	fr : French
-	es : spanish
-	it : Italian
-	bg : Bulgarian
-	cs : czech
-	tr : Turkish
-	ro : Romanian
-	sr : Serbian
+ -  ar Arabic
+ -  az Azerbaijani
+ -  be Belarusian
+ -  bg Bulgarian
+ -  bs Bosnian
+ -  ca Catalan
+ -  cs Czech
+ -  da Danish
+ -  de German
+ -  el Greek
+ -  en English
+ -  es Spanish
+ -  et Estonian
+ -  fi Finnish
+ -  fr French
+ -  he Hebrew
+ -  hr Croatian
+ -  hu Hungarian
+ -  hy Armenian
+ -  id Indonesian
+ -  is Icelandic
+ -  it Italian
+ -  ka Georgian
+ -  lt Lithuanian
+ -  lv Latvian
+ -  mk Macedonian
+ -  ms Malay
+ -  mt Maltese
+ -  nl Dutch
+ -  no Norwegian
+ -  pl Polish
+ -  pt Portuguese
+ -  ro Romanian
+ -  ru Russian
+ -  sk Slovak
+ -  sl Slovenian
+ -  sq Albanian
+ -  sr Serbian
+ -  sv Swedish
+ -  th Thai
+ -  tr Turkish
+ -  uk Ukrainian
+ -  vi Vietnamese
+ -  zh Chinese
 
 ==================================================	"""
 		tolang = raw_input()
